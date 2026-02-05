09bbb1df49a88695decc42aa4e1090f7642f1160
Vasyl Yefimenko <rednez@gmail.com>
2026-02-06 00:07:41 +0100

    enable routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,23 @@ import { Routes } from '@angular/router';
 import { PageNotFound } from './features/page-not-found';
 
 export const routes: Routes = [
-  // {
-  //   path: 'dashboard',
-  //   loadComponent: () =>
-  //     import('./features/dashboard').then((c) => c.Dashboard),
-  // },
-  // {
-  //   path: 'transactions',
-  //   loadComponent: () =>
-  //     import('./features/transactions').then((c) => c.Transactions),
-  // },
-  // {
-  //   path: 'wallet',
-  //   loadComponent: () => import('./features/wallet').then((c) => c.Wallet),
-  // },
-  // {
-  //   path: 'goals',
-  //   loadComponent: () => import('./features/goals').then((c) => c.Goals),
-  // },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard').then((c) => c.Dashboard),
+  },
+  {
+    path: 'transactions',
+    loadComponent: () =>
+      import('./features/transactions').then((c) => c.Transactions),
+  },
+  {
+    path: 'wallet',
+    loadComponent: () => import('./features/wallet').then((c) => c.Wallet),
+  },
+  {
+    path: 'goals',
+    loadComponent: () => import('./features/goals').then((c) => c.Goals),
+  },
   { path: '**', component: PageNotFound },
 ];
