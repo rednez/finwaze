742c12f3d9aec586b79a449aafd34539d0abac2f
Vasyl Yefimenko <rednez@gmail.com>
2026-04-04 20:21:00 +0200

    fix(analytics):  remove p-button wrapper from financial-summary-card-button, keeping just the inner div

--- src/app/features/analytics/ui/financial-summary-card/financial-summary-card-button/financial-summary-card-button.ts
+++ src/app/features/analytics/ui/financial-summary-card/financial-summary-card-button/financial-summary-card-button.ts
@@ -5,21 +5,19 @@ import { ButtonModule } from 'primeng/button';
   selector: 'app-financial-summary-card-button',
   imports: [ButtonModule],
   template: `
-    <p-button variant="text" size="small" rounded>
-      <div class="flex items-center gap-2">
-        <div class="bg-primary-100 dark:bg-surface-800 flex p-2 rounded-full">
-          <span
-            class="material-symbols-rounded dark:text-primary-500 text-[18px]!"
-          >
-            {{ icon() }}
-          </span>
-        </div>
-        <span class="text-gray-800 dark:text-surface-300 font-medium">
-          <span>{{ count() }}</span>
-          <span class="ml-0.5">{{ label() }}</span>
+    <div class="flex items-center gap-2">
+      <div class="bg-primary-100 dark:bg-surface-800 flex p-2 rounded-full">
+        <span
+          class="material-symbols-rounded dark:text-primary-500 text-[18px]!"
+        >
+          {{ icon() }}
         </span>
       </div>
-    </p-button>
+      <span class="text-sm text-gray-800 dark:text-surface-300 font-medium">
+        <span>{{ count() }}</span>
+        <span class="ml-0.5">{{ label() }}</span>
+      </span>
+    </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
