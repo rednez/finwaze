0ee6c5db9761fea41bcec23547bd02ab1655c985
Vasyl Yefimenko <rednez@gmail.com>
2026-04-05 22:42:15 +0200

    feat(build): add staging configuration

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabase: {
+    url: 'https://askfhfgnymgodbyssuzy.supabase.co',
+    publicKey: 'sb_publishable_BoRhpCpK_k-idsaCICa9SQ_tVay6VyJ',
+  },
+};
