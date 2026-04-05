ba4c2a0aba93b3a45e15807109bb1f47d4394be6
Vasyl Yefimenko <rednez@gmail.com>
2026-04-05 23:40:33 +0200

    fix(environment): update Supabase configuration with correct URL and public key for PROD

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   supabase: {
-    url: '',
-    publicKey: '',
+    url: 'https://njhdphddoqxoivbbknse.supabase.co',
+    publicKey: 'sb_publishable_MWCcE0Ysohju09-Z-VT9aw_UTz0a8Ly',
   },
 };
